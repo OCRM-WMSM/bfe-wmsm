f89e4a925652ac3e2d2e95bdad69cf8da14b4c97
unknown <st-wg-hzw14176@BOCVIEW.COM>
2020-01-13 09:34:58 +0800

    页面布局调整，去掉无用代码

--- src/common/BocnetContent.vue
+++ src/common/BocnetContent.vue
@@ -1,47 +1,25 @@
 <template>
   <div class="page-container">
     <div class="page-container-background" v-if="!fullScreen">
-      <div class="page-container__main container" >
-        <div class="row" style="margin-top:10px;margin-bottom:10px;">
+      <div class="page-container__main row">
+        <div class="page-container__header">
           <bfe-breadcrumb separator=">" style="font-size:16px">
             <bfe-breadcrumb-item :to="{ path: '/' }" >首页</bfe-breadcrumb-item>
             <bfe-breadcrumb-item>活动管理</bfe-breadcrumb-item>
             <bfe-breadcrumb-item>活动详情</bfe-breadcrumb-item>
           </bfe-breadcrumb>
          </div>
-         <div class="row">
+         <div class="page-container__content">
            <router-view></router-view>
         </div>
       </div>
     </div>
-    <router-view v-if="fullScreen"></router-view>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'BocnetContent',
-    data() {
-      return {
-        fullScreen: true  // 默认不全屏
-      };
-    },
-    beforeMount() {
-      if(this.$route.meta.fullScreen) {
-        this.fullScreen = true;
-      }else{
-        this.fullScreen = false;
-      }
-    },
-    watch: {
-      $route: function() {
-        if(this.$route.meta.fullScreen) {
-          this.fullScreen = true;
-        }else{
-          this.fullScreen = false;
-        }
-      }
-    }
+    name: 'BocnetContent'
   }
 
 </script>
@@ -54,11 +32,20 @@
   background-image: url("../assets/img/bg.png");
   background-attachment: fixed ;
   background-position: center;
-  min-height: 700px;
+  min-height: 780px;
 }
 .page-container__main{
-  width: 1002px;
+  width: 100%;
   padding: 0px;
+}
+.page-container__header{
+  margin-top:10px;
+  margin-left:3%
+}
+.page-container__content{
+  margin-top:10px;
+  margin-left:3%;
+  margin-right:2%
 }
 
 </style>
